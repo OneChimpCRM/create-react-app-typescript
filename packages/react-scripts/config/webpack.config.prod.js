9190b4df80156520e225b64a899b88c4cfd3cbfa
CLooker <chad.looker@gmail.com>
2018-04-29 09:36:54 -0400

    internal: modified fork of create-react-app-typescript so that styled components are debuggable upon inspection in chrome console

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -170,6 +170,7 @@ module.exports = {
               babelrc: false,
               presets: [require.resolve('babel-preset-react-app')],
               // @remove-on-eject-end
+              plugins: [require.resolve('babel-plugin-styled-components')],
               compact: true,
             },
           },
